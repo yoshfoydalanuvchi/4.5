94d0611f4c079477bf9667d34a9aa625195705b7
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-09 20:39:30 +0500

    none

--- js/index.js
+++ js/index.js
@@ -1,8 +1,92 @@
-let i = 0;
-while (i < 50) {
-  i++;
-  console.log(i);
-  if (i == 30) {
-    break;
-  }
-}
+// break/continue ga oid masalalar
+
+// 1-masala
+// let  count = 0;
+// while ( count < 50) {
+//    count++;
+//   console.log(count);
+//   if (count == 30) {
+//     break;
+//   }
+// }
+
+// 2-masala
+// let count = 0;
+// while (count < 50) {
+//   count++;
+//   if (count % 5 == 0) {
+//     continue;
+//   }
+//   console.log(count);
+// }
+
+// 3-masala
+// let  count = 0;
+// while ( count < 100) {
+//    count++;
+//   console.log(count);
+//   if (count == 75) {
+//     break;
+//   }
+// }
+
+// 4-masala
+// let count = 0;
+// while (count < 30) {
+//   count++;
+//   if (count % 3 == 0) {
+//     continue;
+//   }
+//   console.log(count);
+// }
+
+// While hamda do…while yordamida ishlanadigan masalalar
+// ### `while` sikl operatoriga oid masalalar:
+
+// 1 - masala;
+// let count = 1;
+// while (count <= 20) {
+//   console.log(count);
+//   count++;
+// }
+
+// 2 - masala;
+// let count = 10;
+// while (count >= 1) {
+//   console.log(count);
+//   count--;
+// }
+
+// 3 - masala;
+// let count = 0;
+// while (count <= 50) {
+//   if (count % 2 == 0) {
+//     console.log(count);
+//   }
+//   count++;
+// }
+
+// 4-masala
+// let count = 0;
+// while (count <= 50) {
+//   if (count % 2 == 1) {
+//     console.log(count);
+//   }
+//   count++;
+// }
+
+// 5-masala
+// let count = 0;
+// let result = 0;
+// while (count <= 100) {
+//   result = result + count;
+//   count++;
+// }
+// console.log(result);
+
+// 6-masala
+// let count = 1;
+// while (count <= 10) {
+//   console.log(count * count);
+//   count++;
+// }
